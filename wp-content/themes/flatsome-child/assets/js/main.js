951e2f8c39926585fc6256fa1ca7ac741b316376
NGOAN <thiengoan@gmail.com>
2023-08-19 10:33:16 +0700

    update

--- wp-content/themes/flatsome-child/assets/js/main.js
+++ wp-content/themes/flatsome-child/assets/js/main.js
@@ -50,3 +50,15 @@ jQuery(document).ready(function($) {
         $('.wpcf7-form').removeClass('processing');
     }, false);
 });
+
+
+window.onscroll = function(e) { 
+    var scrollY = window.pageYOffset || document.documentElement.scrollTop;
+    var header = document.querySelector('header');
+
+    scrollY <= this.lastScroll 
+      ? header.style.visibility = 'visible'
+      : header.style.visibility = 'hidden'; 
+
+    this.lastScroll = scrollY ;
+}
